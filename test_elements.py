a279525e14d08f0a108bd383ecef71e650745e7e
niharikavaka <89569094+niharikavaka@users.noreply.github.com>
2025-03-13 11:29:04 +0530

    Create test_elements.py

---
+++ test_elements.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+def test_elements():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)  # Launch Chromium browser
+        page = browser.new_page()
+        
+        page.goto('https://mira.ccs.arubathena.com/home') 
+        
+        page.locator('input[id="idp-discovery-username"]').fill('GitHub Actions') 
+        assert page.locator('input[id="idp-discovery-username"]').input_value() == 'GitHub Actions'
+        browser.close()
