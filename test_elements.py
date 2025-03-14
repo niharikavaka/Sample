d74a93444037c2997baf9a8612d48e522746193a
niharikavaka <89569094+niharikavaka@users.noreply.github.com>
2025-03-14 15:16:37 +0530

    sample2

--- test_elements.py
+++ test_elements.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 
-def test_elements():
+def test_elements1():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)  # Launch Chromium browser
         page = browser.new_page()
